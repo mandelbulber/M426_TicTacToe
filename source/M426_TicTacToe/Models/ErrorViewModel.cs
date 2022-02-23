f44fc441074d753e34bb3ef52bd6d3a8b924f5f2
Andrin Eugster <aeugster@online.gibz.ch>
2022-02-23 11:05:03 +0100

    Projektdateien hinzufügen.

---
+++ source/M426_TicTacToe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace M426_TicTacToe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
