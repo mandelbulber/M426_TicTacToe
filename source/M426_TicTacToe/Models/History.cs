da3dd8a50e3d3e785cb5c432fba8653d44e9f59f
Marcel Steiner <marcelsteiner@bluewin.ch>
2022-03-02 11:05:47 +0100

    Add Properties of History Model

--- source/M426_TicTacToe/Models/History.cs
+++ source/M426_TicTacToe/Models/History.cs
@@ -7,5 +7,10 @@ namespace M426_TicTacToe.Models
 {
     public class History
     {
+        public int Id { get; set; }
+        public virtual string Player1 { get; set; }
+        public virtual string Player2 { get; set; }
+        public string Game { get; set; }
+
     }
 }
