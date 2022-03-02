0804ebcdfdd87e4f5ed65a11ec65bbfb33a1465d
Tim Arnold <tarnold@online.gibz.ch>
2022-03-02 19:33:57 +0100

    SignalR setup

---
+++ source/M426_TicTacToe/Hubs/TicTacToeHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace M426_TicTacToe.Hubs
+{
+    public class TicTacToeHub : Hub
+    {
+    }
+}
--- source/M426_TicTacToe/Startup.cs
+++ source/M426_TicTacToe/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using M426_TicTacToe.Hubs;
 
 namespace M426_TicTacToe
 {
@@ -35,6 +36,7 @@ namespace M426_TicTacToe
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +67,7 @@ namespace M426_TicTacToe
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
+                endpoints.MapHub<TicTacToeHub>("/chatHub");
             });
         }
     }
