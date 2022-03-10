db2f458e89b1551f3594015be8a124204a02a73a
Andrin Eugster <aeugster@online.gibz.ch>
2022-03-10 13:33:14 +0100

    Create new HistoryController and Index View

---
+++ source/M426_TicTacToe/Controllers/HistoryController.cs
@@ -0,0 +1,15 @@
+﻿using M426_TicTacToe.Enums;
+using M426_TicTacToe.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace M426_TicTacToe.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
