c08677082f41820f6234990ca708ce1bce9e658e
Tim Arnold <tarnold@online.gibz.ch>
2022-03-11 20:17:17 +0100

    Implemented unit tests

---
+++ source/M426_TicTacToe_Testing/GameUnitTests.cs
@@ -0,0 +1,72 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using M426_TicTacToe.Models;
+using M426_TicTacToe.Enums;
+using M426_TicTacToe.Hubs;
+using Newtonsoft.Json;
+
+namespace M426_TicTacToe_Testing
+{
+    [TestClass]
+    public class GameUnitTests
+    {
+        #region Game.IsPlayer1()
+
+        [TestMethod]
+        public void IsPlayer1TrueTest()
+        {
+            // Arrange
+            FieldState[] fieldStates = new FieldState[9]
+            {
+                FieldState.none,
+                FieldState.player1,
+                FieldState.none,
+                FieldState.player2,
+                FieldState.none,
+                FieldState.none,
+                FieldState.none,
+                FieldState.none,
+                FieldState.none
+            };
+            Game game = new()
+            {
+                Board = JsonConvert.SerializeObject(fieldStates)
+            };
+
+            // Act
+            bool isPlayer1 = game.IsPlayer1();
+
+            // Assert
+            Assert.IsTrue(isPlayer1);
+        }
+
+        [TestMethod]
+        public void IsPlayer1FalseTest()
+        {
+            // Arrange
+            FieldState[] fieldStates = new FieldState[9]
+            {
+                FieldState.player2,
+                FieldState.none,
+                FieldState.none,
+                FieldState.none,
+                FieldState.none,
+                FieldState.none,
+                FieldState.player1,
+                FieldState.none,
+                FieldState.player1
+            };
+            Game game = new()
+            {
+                Board = JsonConvert.SerializeObject(fieldStates)
+            };
+
+            // Act
+            bool isPlayer1 = game.IsPlayer1();
+
+            // Assert
+            Assert.IsFalse(isPlayer1);
+        }
+
+        #endregion
+    }
+}
