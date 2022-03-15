2745f93eeb2a13640032fd6878fc18ee0a1530b7
Andrin Eugster <aeugster@online.gibz.ch>
2022-03-15 20:32:29 +0100

    Add Controller backend logic

--- source/M426_TicTacToe/Controllers/HistoryController.cs
+++ source/M426_TicTacToe/Controllers/HistoryController.cs
@@ -1,15 +1,49 @@
-﻿using M426_TicTacToe.Enums;
+﻿using M426_TicTacToe.Data;
+using M426_TicTacToe.Enums;
+using M426_TicTacToe.Models;
 using M426_TicTacToe.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace M426_TicTacToe.Controllers
 {
     public class HistoryController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HistoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Searches the db for all games where the currently logged in user is part of
+        /// Converts all found games into GameViewModels and returns the History View with the games as param
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
-            return View();
+            List<GameViewModel> games = new List<GameViewModel>();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId != null)
+            {
+                foreach (Game game in _dbContext.Games.Where(x => x.Player1 == userId || x.Player2 == userId))
+                {
+                    GameViewModel gameViewModel = new()
+                    {
+                        Id = game.Id,
+                        Player1 = game.Player1,
+                        Player2 = game.Player2,
+                        TimeStamp = game.TimeStamp,
+                        GameState = (GameState)game.Winner,
+                        Board = JsonConvert.DeserializeObject<FieldState[]>(game.Board)
+                    };
+                    games.Add(gameViewModel);
+                }
+            }
+            return View(games);
         }
     }
 }
--- source/M426_TicTacToe/Models/Game.cs
+++ source/M426_TicTacToe/Models/Game.cs
@@ -7,13 +7,11 @@ namespace M426_TicTacToe.Models
 {
     public class Game
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public virtual string Player1 { get; set; }
         public virtual string Player2 { get; set; }
         public int Winner { get; set; }
         public DateTime TimeStamp { get; set; }
         public string Board { get; set; }
-
-
     }
 }
