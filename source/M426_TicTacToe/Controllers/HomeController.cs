f757278c7c4e68884d1f75bdb0f438ead9c7edf4
nschuermannschool <nschuermann@online.gibz.ch>
2022-03-16 13:53:19 +0100

    list joinable games on homepage

--- source/M426_TicTacToe/Controllers/HomeController.cs
+++ source/M426_TicTacToe/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
-﻿using M426_TicTacToe.Models;
+﻿using M426_TicTacToe.Data;
+using M426_TicTacToe.Enums;
+using M426_TicTacToe.Models;
+using M426_TicTacToe.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace M426_TicTacToe.Controllers
@@ -12,14 +17,37 @@ namespace M426_TicTacToe.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated) 
+            {
+                List<Game> dbGames = _context.Games.ToList();
+                List<GameViewModel> games = new List<GameViewModel>();
+
+                foreach (var game in dbGames)
+                {
+                    GameViewModel gvm = new GameViewModel();
+                    gvm.Id = game.Id;
+                    gvm.Player1 = _context.Users.FirstOrDefault(x => x.Id == game.Player1).UserName;
+                    if (_context.Users.FirstOrDefault(x => x.Id == game.Player2) != null) 
+                    {
+                        gvm.Player2 = _context.Users.FirstOrDefault(x => x.Id == game.Player2).UserName;
+                    }
+                    gvm.TimeStamp = game.TimeStamp;
+                    gvm.GameState = (GameState)game.Winner;
+                    gvm.Board = JsonConvert.DeserializeObject<FieldState[]>(game.Board);
+                    games.Add(gvm);
+                }
+                return View(games);
+            }
             return View();
         }
 
